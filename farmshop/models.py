8ac8f651f6b7428b23f5d547d1db26c3a1826252
Dyl06 <dylhndrsn@gmail.com>
2023-05-16 15:49:24 +0000

    Creation of first model

--- farmshop/models.py
+++ farmshop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Product(models.Model):
+    product = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+    product_featured_image = CloudinaryField('image', default='placeholder')
+    product_description = models.TextField()
+    items = models.CharField(max_length=100, unique=True)
+    items_featured_image = CloudinaryField('image', default='placeholder')
+    items_description = models.TextField()
+    weight = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    likes = models.ManyToManyField(User, related_name='item_likes', blank=True)
+
+    def __str__(self):
+        return self.items
+
+    def accumulated_likes(self):
+        return self.likes.count()
