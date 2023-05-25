5073cbd3613a919d6bd33fbadc8a38648ac21b73
Dyl06 <dylhndrsn@gmail.com>
2023-05-25 16:01:52 +0000

    Add images to site

--- field_to_fork/settings.py
+++ field_to_fork/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['f2f-farmshop.herokuapp.com', 'localhost', '8000-dyl06-fieldtofork-caarqaix63h.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['f2f-farmshop.herokuapp.com', 'localhost']
 
 
 # Application definition
