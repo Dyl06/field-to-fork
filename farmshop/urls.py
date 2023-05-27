b3675031fd787345b3ccde3c1774d9ab472eb285
Dyl06 <dylhndrsn@gmail.com>
2023-05-27 16:51:24 +0000

    Finish css style for home page

--- farmshop/urls.py
+++ farmshop/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
+    path('home/', views.HomePage.as_view(), name='home'),
     path('products/<str:category>', views.CategoryList.as_view(),
          name='product_list'),
     path('my_orders/', views.OrderList.as_view(), name='orders')
