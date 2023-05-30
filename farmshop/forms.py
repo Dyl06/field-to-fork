f319c3036185715a675676387cf861177404cc10
Dyl06 <dylhndrsn@gmail.com>
2023-05-30 22:19:49 +0000

    Delete all commented out code and resolve problems and errors in code.

--- farmshop/forms.py
+++ farmshop/forms.py
@@ -8,13 +8,13 @@ class NewUserForm(UserCreationForm):
 
 
 class Meta:
-	model = User
-	fields = ("username", "email", "password1", "password2")
+    model = User
+    fields = ("username", "email", "password1", "password2")
 
 
 def save(self, commit=True):
-	user = super(NewUserForm, self).save(commit=False)
-	user.email = self.cleaned_data['email']
-	if commit:
-		user.save()
-	return user
+    user = super(NewUserForm, self).save(commit=False)
+    user.email = self.cleaned_data['email']
+    if commit:
+        user.save()
+    return user
