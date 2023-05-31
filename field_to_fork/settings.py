749b5686898e8b349100ad8ce294e517cbab7e26
Dyl06 <dylhndrsn@gmail.com>
2023-05-31 10:19:22 +0000

    install whitenoise

--- field_to_fork/settings.py
+++ field_to_fork/settings.py
@@ -55,6 +55,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
